b5a8d74cd7535b8d2185d759766af701ce66e6e3
Pradeep Gollakota <pradeepg26@gmail.com>
2018-12-02 20:40:57 -0800

    implement 2015/day4

---
+++ 2015/day4/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+)
+
+func Hash(key string, i int) string {
+	data := []byte(fmt.Sprintf("%s%d", key, i))
+	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func Mine(key string, numLeading int) int {
+	prefix := strings.Repeat("0", numLeading)
+	for i := 1; i < 2000000000; i++ {
+		hash := Hash(key, i)
+		if strings.HasPrefix(hash, prefix) {
+			return i
+		}
+	}
+	panic("must find num")
+}
+
+func main() {
+	fmt.Println(Mine("abcdef", 5))
+	fmt.Println(Mine("pqrstuv", 5))
+	fmt.Println(Mine("bgvyzdsv", 5))
+	fmt.Println(Mine("bgvyzdsv", 6))
+}
